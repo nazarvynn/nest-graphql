03d7344fbcd3b83f77ecf12fbdcc435c5ce6946b
Nazar Vynnytskyi <nvynnytskyi@jappware.com>
2022-04-11 13:50:15 +0300

    Add react users

--- frontend-react-apollo/src/App.tsx
+++ frontend-react-apollo/src/App.tsx
@@ -1,25 +1,25 @@
-import React from 'react';
-import logo from './logo.svg';
-import './App.css';
+import React from "react";
+import {
+  BrowserRouter as Router,
+  Routes,
+  Route,
+  Navigate,
+} from "react-router-dom";
+import Users from "./pages/Users";
+
+import "primereact/resources/themes/saga-blue/theme.css";
+import "primereact/resources/primereact.min.css";
+import "primeicons/primeicons.css";
+import "primeflex/primeflex.css";
 
 function App() {
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.tsx</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
+    <Router>
+      <Routes>
+        <Route path="/users" element={<Users />} />
+        <Route path="/" element={<Navigate replace to="/users" />} />
+      </Routes>
+    </Router>
   );
 }
 
